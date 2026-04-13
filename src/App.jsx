b8090befe1a84adfdf0c46748ca21ac6a33fada0
Naheun Alam <naheunpranto258@gmail.com>
2026-04-13 18:14:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import "./App.css";
+
+function App() {
+  function handleClick() {
+    alert("I am clicked");
+  }
+
+  const handleClick3 = () => {
+    alert("I am clicked 3");
+  };
+
+  const handleAdd5 = (num) => {
+    const newNum = num + 5;
+    alert(newNum);
+  };
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      {/* <button onclick="handleClick()">Click Me</button>     */}
+      <button className="btn" onClick={handleClick}>
+        Click Me
+      </button>
+
+      <button
+        className="btn"
+        onClick={function handleClick2() {
+          alert("I am clicked 2");
+        }}
+      >
+        Click Me - 2
+      </button>
+
+      <button className="btn" onClick={handleClick3}>
+        Click Me - 3
+      </button>
+
+      <button
+        className="btn"
+        onClick={() => {
+          alert("I am clicked 4");
+        }}
+      >
+        Click Me - 4
+      </button>
+
+      <button className="btn" onClick={() => handleAdd5(10)}>Click Add 5</button>
+    </>
+  );
+}
+
+export default App;
